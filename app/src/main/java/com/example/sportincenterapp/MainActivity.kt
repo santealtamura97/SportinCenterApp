44e43e1b1910facdd974ebd3d6c8f4023e6c074e
aforino95 <andrea.forino@edu.unito.it>
2021-05-06 18:18:39 +0200

    aggiustamenti iniziali

--- app/src/main/java/com/example/sportincenterapp/MainActivity.kt
+++ app/src/main/java/com/example/sportincenterapp/MainActivity.kt
@@ -7,7 +7,6 @@ package com.example.sportincenterapp
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
