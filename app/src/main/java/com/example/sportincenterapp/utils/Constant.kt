beb77ec0276cba3fe13b6bbbd147a6dc9eddccb3
Sante <sante_altamura97@hotmail.com>
2021-05-14 13:14:44 +0200

    Login e comunicazione con docker container

--- app/src/main/java/com/example/sportincenterapp/utils/Constant.kt
+++ app/src/main/java/com/example/sportincenterapp/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.example.sportincenterapp.utils
 object Constant {
 
     //Endpoints
-    const val BASE_URL = "http://192.168.1.97:8080/api/"
+    const val BASE_URL = "http://10.0.2.2:8080/api/"
     const val LOGIN_URL = "auth/signin"
 
 }
